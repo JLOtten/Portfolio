af8688a7690a0a1928b17dae2b56320faf34a660
jlotten <jenna.otten@gmail.com>
2023-09-19 18:53:05 -0700

    try window.open for links

--- components/project.tsx
+++ components/project.tsx
@@ -25,7 +25,7 @@ export default function Project({
   const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
 
   const handleClick = () => {
-    window.location.href = url;
+    window.open(url);
   };
 
   return (
