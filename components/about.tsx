11c0823106a6c196c0cfed12bd23cc06c4b63c9c
Jenna Otten <108947258+JLOtten@users.noreply.github.com>
2023-10-04 12:36:46 -0700

    Skillcrush guidlines changes (#9)

--- components/about.tsx
+++ components/about.tsx
@@ -20,34 +20,25 @@ export default function About() {
       <SectionHeading>About Me</SectionHeading>
 
       <p className="mb-3 text-left" style={{ textIndent: "1.5em" }}>
-        I'm a curious life-long learner with a background in{" "}
-        <span className="font-medium">Education</span>. I enjoyed over 10 years
-        teaching high school, elementary and adults the subjects of math,
-        science and English as a second language. As a parent during pandemic
-        distance learning, I had some great user experiences with education technology. I
-        was inspired to use many of those applications in my own classroom when
-        we returned to in-person learning.
+       I’m a <span className="font-medium">Front-end Developer</span> with a background in education, 
+       specializing in accessible and responsive web apps. I enjoyed over 10 years of teaching math, 
+       science, and English as a second language to various age groups: ranging from 5 to 65.
       </p>
 
-      <p className="text-left" style={{ textIndent: "1.5em" }}>
-        I made a <span className="italic">brave</span> choice to enroll in a
-        bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>. Through
-        daily remote pair programming, I learned{" "}
-        <span className="underline">Python, PostgreSQL and Bootstrap</span>.
-        Following that, I received a scholarship to earn a{" "}
-        <span className="font-medium">Google UX Design Certification</span>,
-        where I used{" "}
-        <span className="underline">
-          Figma, AdobeXD and UX Research & Design principles
-        </span>
-        . I simultaneously completed a front-end program, to deepen my knowledge
-        of <span className="underline">JavaScript, HTML and CSS</span> and
-        frameworks like <span className="underline">React</span>. When I'm not
-        learning new technologies or coding, I'm likely walking my dogs or
-        cheering at my kids' sports events. I am currently looking for a{" "}
-        <span className="font-medium">full-time </span> position as a{" "}
-        <span className="font-medium"> software developer</span>.
+      <p className="mb-3 text-left" style={{ textIndent: "1.5em" }}>
+       During the pandemic, I discovered a passion for educational technology
+       and decided to pursue software engineering. I completed a <span className="">software engineering 
+       bootcamp, obtained a Google UX Design Certification,</span> and enhanced my skills in {" "}
+       <span className="underline">JavaScript, HTML, CSS, Python, PostgreSQL and React</span> while using tools like
+       {" "}<span className="underline">Git & GitHub, VS Code, Figma, Adobe XD</span> and more. I'm dedicated to designing, 
+       building and improving technology to simplify the lives of everyone, and
+       have a particular empathy for parents, teachers and families.
+      </p>
+
+      <p className="text-left" style={{ textIndent: "1.5em"}}>
+      When I'm not learning new technologies or coding, I'm likely walking my dogs
+      or cheering at my kids' sports events. I am currently looking for a 
+      full-time position as a Front-end Developer.
       </p>
     </motion.section>
   );
